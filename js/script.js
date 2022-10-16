8c50eb65f48e3e3397d49b0878265ef92cae8de6
Editson <107451363+editsonT@users.noreply.github.com>
2022-10-16 22:01:07 -0100

    add files

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');    
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); 
+    //getting mario and replacing px(str) with nothing and + to convert it to number
+    console.log(marioPosition);
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+
+document.addEventListener('keydown', jump);
+//ao precionar uma tecla, a funcao jump e executada
+    //vai adicionar a classe jump, a animacao e executada
+    //animacao termina, a classe e removida | 500 pq = jump animatio0 time 
